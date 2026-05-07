84a5c0a1836608de274760d1c6cd17420faec885
Kaung Myat Han <kaungmyathan2222@gmail.com>
2026-05-07 12:55:52 +0700

    feat(): implement preorder traversal for binary tree

---
+++ binary_tree_preorder_traversal.go
@@ -0,0 +1,12 @@
+package main
+
+func preorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	if root == nil {
+		return result
+	}
+	result = append(result, root.Val)
+	result = append(result, preorderTraversal(root.Left)...)
+	result = append(result, preorderTraversal(root.Right)...)
+	return result
+}
