1b993f61c0123dfb652d7e0f0aa07c699c1fb624
Kaung Myat Han <kaungmyathan2222@gmail.com>
2026-05-07 13:01:07 +0700

    feat(): implement postorder traversal for binary tree

--- binary_tree_postorder_traversal.go
+++ binary_tree_postorder_traversal.go
@@ -1,9 +1,13 @@
+package main
+
 func postorderTraversal(root *TreeNode) []int {
 	result := []int{}
 	if root == nil {
 		return result
 	}
 
+	result = append(result, postorderTraversal(root.Left)...)
+	result = append(result, postorderTraversal(root.Right)...)
 	result = append(result, root.Val)
 	return result
 }
